806d0c3955283a211bf5199e3138b26ca82ff454
mrtedn21 <bezgin.sasha06@gmail.com>
2021-09-29 21:22:14 +0700

    make show command that accept one argument - username and returns all wishes of this user

--- bot.py
+++ bot.py
@@ -27,12 +27,38 @@ def add(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='ok')
 
 
+def show(update, context):
+    args = context.args
+
+    if not len(args):
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='write username, whose wishes you want to watch'
+        )
+        return
+
+    username_for_showing = args[0]
+
+    resulting_message = ''
+
+    with engine.connect() as conn:
+        select_query = text("SELECT * FROM wish WHERE username = :username")
+        result = conn.execute(select_query, username=username_for_showing)
+        for wish in result:
+            resulting_message = f'{resulting_message}\n{wish[1]}'
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=resulting_message)
+
+
 telegram_bot_token = os.getenv('WISH_APP_TOKEN')
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('add', add)
-dispatcher.add_handler(start_handler)
+add_handler = CommandHandler('add', add)
+dispatcher.add_handler(add_handler)
+
+show_handler = CommandHandler('show', show)
+dispatcher.add_handler(show_handler)
 
 updater.start_polling()
