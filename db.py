8e970b750d2e1ffa1d0c955832cf900288b86961
mrtedn21 <bezgin.sasha06@gmail.com>
2021-09-30 21:22:36 +0700

    refactor

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 
-create_wish_table_query = \
-    'CREATE TABLE wish(' \
-    '   first_name VARCHAR(256),' \
-    '   text VARCHAR(256),' \
-    '   PRIMARY KEY(first_name, text)' \
-    ')'
+create_wish_table_query = """
+    CREATE TABLE wish(
+       first_name VARCHAR(256),
+       text VARCHAR(256),
+       PRIMARY KEY(first_name, text)
+    )"""
 
 engine = create_engine('postgresql+psycopg2://mrtedn:123@localhost/wish_db')
 
