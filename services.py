2ce4a6f6855f17e3c22e309a08b989eceee4cb2b
mrtedn21 <bezgin.sasha06@gmail.com>
2022-05-02 14:56:43 +0700

    add show_command function

--- services.py
+++ services.py
@@ -2,6 +2,7 @@
 from models.database import Wish
 from models.database import async_session
 from sqlalchemy import insert
+from sqlalchemy import select
 
 
 async def create_wish(username, text):
@@ -10,3 +11,17 @@ async def create_wish(username, text):
             insert(Wish).values(username=username, text=text)
         )
         await session.commit()
+
+
+async def get_wishes_by_username(username: str):
+    async with async_session() as session:
+        result = await session.execute(
+            select(Wish.text).where(Wish.username == username)
+        )
+        wishes = result.fetchall()
+        # result of fetchall is, for example:
+        # [('iphone',), ('corne',), ('gtx 3070',)]
+        # i make this list to:
+        # ['iphone', 'corne', 'gtx 3070']
+        wishes = [wish[0] for wish in wishes]
+        return wishes
--- worker.py
+++ worker.py
@@ -7,8 +7,11 @@ from api_functions import send_message
 from models.database import engine
 from models.rabbit import RabbitMessage
 from services import create_wish
+from services import get_wishes_by_username
 
 QUEUE_NAME = 'wish'
+# usual \n doesn't work in url
+NEW_LINE_CHARACTER = '%0A'
 
 
 async def add_command(
@@ -27,6 +30,19 @@ async def add_command(
     )
 
 
+async def show_command(
+        chat_id: int,
+        username: str,
+        session: aiohttp.ClientSession):
+    wishes = await get_wishes_by_username(username)
+    result_text = NEW_LINE_CHARACTER.join(wishes)
+    await send_message(
+        session=session,
+        chat_id=chat_id,
+        text=f'wishes of user "{username}" is:{NEW_LINE_CHARACTER}{result_text}',
+    )
+
+
 async def message_handler(
         rb_message: RabbitMessage,
         session: aiohttp.ClientSession) -> None:
@@ -37,11 +53,17 @@ async def message_handler(
     # remove elements = spaces. Need for case if user will
     # write commands with several spaces between commands
     commands = [command for command in commands if command]
-    if commands[0] == 'add':
+    if commands[0].lower() == 'add':
         await add_command(
             chat_id=rb_message.chat_id,
             username=rb_message.username,
             wish=' '.join(commands[1:]),
+            session=session
+        )
+    elif commands[0].lower() == 'show':
+        await show_command(
+            chat_id=rb_message.chat_id,
+            username=rb_message.username,
             session=session
         )
 
